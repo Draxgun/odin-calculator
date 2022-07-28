92b6ba8608515af66f3b4017be6574e3fc46b4ed
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-28 10:52:47 -0600

    operate function done

--- javascript.js
+++ javascript.js
@@ -17,11 +17,33 @@ let divide = (a,b) =>{
 }
 
 /* Tests */
-let a = 1;
+/* let a = 1;
 let b = 2;
 
 console.log(add(a,b))
 console.log(substract(a,b))
 console.log(multiply(a,b))
-console.log(divide(a,b))
+console.log(divide(a,b)) */
+
+/* Operate */
+let operators = ['+','-','*','/']
+
+/* Function that operates */
+let operate = (a,b,operator) =>{
+    return operators.includes(operator) ? checkOperator(a,b,operator):false
+}
+
+/* Checks which operator is used */
+let checkOperator = (a,b,operator) =>{
+    if(operator==='+'){
+        return add(a,b);
+    }else if (operator==='-'){
+        return substract(a,b);
+    }else if (operator==='*'){
+        return multiply(a,b);
+    }else if (operator==='/'){
+        return divide(a,b);
+    }
+}
+
 
