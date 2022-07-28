d6bcd99ae390609b34153d76559679da40b47a32
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-28 10:34:32 -0600

    math changes

---
+++ javascript.js
@@ -0,0 +1,27 @@
+/* Mathematical functions */
+/* Sum */
+let add = (a,b) =>{
+    return a+b
+}
+/* substraction */
+let substract = (a,b) =>{
+    return a-b
+}
+/* multiply */
+let multiply = (a,b) =>{
+    return a*b
+}
+/* divide */
+let divide = (a,b) =>{
+    return a/b
+}
+
+/* Tests */
+let a = 1;
+let b = 2;
+
+console.log(add(a,b))
+console.log(substract(a,b))
+console.log(multiply(a,b))
+console.log(divide(a,b))
+
